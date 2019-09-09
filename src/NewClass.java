4cbd9fce928f0fbf040fc0fb655b137c3ffbbd36
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2019-09-09 10:09:47 +0200

    Initial commit

---
+++ src/NewClass.java
@@ -0,0 +1,37 @@
+public class NewClass {
+    public static void main(String[] args) {
+
+        //primitive datatype
+        byte a = 127;
+        short c = 1001;
+        int b = 1024;
+        long i = 10000;
+
+        b = a + c;
+
+        //decimal number
+        double d = 2.7;
+        float e = 5.0f;
+       //when you write float you have to add an f at the end of a code.
+
+        //boolean
+        boolean f = true;
+        boolean g = false;
+
+        //string
+        String h = "Pølse";
+
+        //Char
+        char i = 'b';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(h);
+
+    }
+}
